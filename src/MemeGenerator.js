6927e3c7e612dbe7dc114f6142956bf09711db7c
felixongati <fegati02@gmail.com>
2020-01-06 12:14:56 +0300

    initial setup

---
+++ src/MemeGenerator.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class MemeGenerator extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is the MemeGenerator</p>
+            </div>
+        );
+    }
+}
+
+export default MemeGenerator;
